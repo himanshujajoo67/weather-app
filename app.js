aca733f35cc66ab06ab1e8067d8a21341b8e6361
himanshujajoo67 <himanshujajoo67@gmail.com>
2018-02-09 20:03:08 -0500

    Added user input for address and encoded the input to get latitude and longitude

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
+
+
+const argv = yargs
+.options({
+  address: {
+    demand: true,
+    alias: 'a',
+    description: 'Address to Fetch weather for',
+    string: true
+  }
+
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request ({
-  url : 'https://www.google.com/maps/api/geocode/json?address=324%20Sussex%20Street%20harrison%20New%20jersey%2007029',
+  url : `https://www.google.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
