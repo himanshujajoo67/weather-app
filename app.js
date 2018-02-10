2852476455631ec585e53f9b369c71da09647688
himanshujajoo67 <himanshujajoo67@gmail.com>
2018-02-09 19:18:30 -0500

    Fetched Formatted address, Latitude and Longitude

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const request = require('request');
 request ({
   url : 'https://www.google.com/maps/api/geocode/json?address=324%20Sussex%20Street%20harrison%20New%20jersey%2007029',
   json: true
-}, (error, response, body) =>{
-  console.log(body);
+}, (error, response, body) => {
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Lattitude ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude ${body.results[0].geometry.location.lng}`);
 });
